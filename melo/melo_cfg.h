21e32dd2a6985c8ca16d4be6748e8d10e96eb828
David Sunshine <davidcsunshine@gmail.com>
2015-11-18 19:39:44 -0500

    :fire: Initial version to remove dependency on third-party v8stdint.h

--- melo/melo_cfg.h
+++ melo/melo_cfg.h
@@ -1,18 +1,18 @@
 /*
  * Copyright (c) 2015 David Sunshine, <http://sunshin.es>
- * 
+ *
  * This file is part of melo.
- * 
+ *
  * melo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * melo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with melo.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -24,11 +24,8 @@
 extern "C" {
 #endif
 
-#ifdef _WIN32
-  #include "v8stdint.h"
-#endif
 /* #include <stdbool.h> */
-/* #include <stdint.h> */
+#include <stdint.h>
 
 #define MELO_CFG_MAX_DATA_LENGTH       100
 #define MELO_CFG_MAX_STACK_SIZE        5
